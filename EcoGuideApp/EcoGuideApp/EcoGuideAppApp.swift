c61f3e55b9d376719866fea1fa8bfc5de3ad0eb2
m77akd <156592729+m77akd@users.noreply.github.com>
2024-06-19 07:05:22 +0200

    project generate

---
+++ EcoGuideApp/EcoGuideApp/EcoGuideAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoGuideAppApp.swift
+//  EcoGuideApp
+//
+//  Created by Muharrem Akdemir on 19.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoGuideAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
