955b409d84be35d7991a93d947d794fb090f623b
m77akd <156592729+m77akd@users.noreply.github.com>
2024-06-19 08:39:21 +0200

    setup with firebase

--- EcoGuideApp/EcoGuideApp/EcoGuideAppApp.swift
+++ EcoGuideApp/EcoGuideApp/EcoGuideAppApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
-struct EcoGuideAppApp: App {
+struct EcoGuideApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 }
